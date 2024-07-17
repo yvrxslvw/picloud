8647336356af24056f013057443f501732dee241
yvrxslvw <yvrxslvv@gmail.com>
2024-07-17 16:31:42 +0300

    Make environments

--- packages/client/src/app/types.d.ts
+++ packages/client/src/app/types.d.ts
@@ -1,2 +1,34 @@
-declare module '*.module.scss';
-declare module '*.png';
+/// <reference types="vite/client" />
+
+declare module '*.module.scss' {
+	interface IClassNames {
+		[className: string]: string;
+	}
+	const classNames: IClassNames;
+	export = classNames;
+}
+
+declare module '*.jpg' {
+	const src: string;
+	export = src;
+}
+declare module '*.jpeg' {
+	const src: string;
+	export = src;
+}
+declare module '*.png' {
+	const src: string;
+	export = src;
+}
+declare module '*.svg' {
+	const src: string;
+	export = src;
+}
+
+interface ImportMetaEnv {
+	readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
