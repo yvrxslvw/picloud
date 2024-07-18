be46f7fd3ccaff679cef75a0a1ece7f3d7902f12
yvrxslvw <yvrxslvv@gmail.com>
2024-07-18 12:28:51 +0300

    Some fixes for dropdown component

--- packages/client/src/shared/UI/Dropdown/Root.tsx
+++ packages/client/src/shared/UI/Dropdown/Root.tsx
@@ -1,16 +1,30 @@
 import { FC, HTMLAttributes, ReactNode, useState } from 'react';
 import cn from 'classnames';
 import cl from './style.module.scss';
+import { timer } from 'shared/utils';
 
 export interface RootProps extends HTMLAttributes<HTMLDivElement> {
 	mainElement: ReactNode;
 }
 
 export const Root: FC<RootProps> = ({ className, mainElement, children, ...props }) => {
-	const [isShown, setIsShown] = useState(false);
+	const [opacity, setOpacity] = useState(0);
+	const [display, setDisplay] = useState('none');
 
-	const onClickHandler = () => {
-		setIsShown(prev => !prev);
+	const toggleMenu = async (state: boolean) => {
+		if (state) {
+			setDisplay('block');
+			await timer(1);
+			setOpacity(1);
+		} else {
+			setOpacity(0);
+			await timer(200);
+			setDisplay('none');
+		}
+	};
+
+	const onClickHandler = async () => {
+		await toggleMenu(opacity === 0);
 	};
 
 	return (
@@ -19,7 +33,9 @@ export const Root: FC<RootProps> = ({ className, mainElement, children, ...props
 				{mainElement}
 			</span>
 
-			<div className={cl.Menu} style={{ opacity: isShown ? 1 : 0 }}>
+			{opacity === 1 && <div className={cl.CloseArea} onClick={onClickHandler} />}
+
+			<div className={cl.Menu} style={{ opacity, display }}>
 				{children}
 			</div>
 		</div>
