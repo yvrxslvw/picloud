ca59b5a143e757e896e12e63074d139ccde9922c
yvrxslvw <yvrxslvv@gmail.com>
2024-08-09 18:26:14 +0300

    Prevent image dragging

--- packages/client/src/features/HeaderAccount/UI/ProfileImageDropdown/index.tsx
+++ packages/client/src/features/HeaderAccount/UI/ProfileImageDropdown/index.tsx
@@ -1,8 +1,8 @@
-import { usePopup } from 'entities/Popup';
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ROUTER_PATHS } from 'shared/constants';
 import { Dropdown } from 'shared/UI';
+import { ROUTER_PATHS } from 'shared/constants';
+import { usePopup } from 'entities/Popup';
 
 interface ProfileImageDropdownProps {
 	imageLink: string | undefined;
@@ -24,8 +24,12 @@ export const ProfileImageDropdown: FC<ProfileImageDropdownProps> = ({ imageLink 
 		createPopup('logout');
 	};
 
+	const onDragHandler = (e: React.DragEvent) => {
+		e.preventDefault();
+	};
+
 	return (
-		<Dropdown.Root mainElement={<img src={imageLink} alt='profile' />}>
+		<Dropdown.Root mainElement={<img src={imageLink} alt='profile' onDragStart={onDragHandler} />}>
 			<Dropdown.Node content='Мой диск' onClick={onClickDriveHandler} />
 			<Dropdown.Node content='Настройки' onClick={onClickSettingsHandler} />
 			<Dropdown.Node content='Выйти из аккаунта' onClick={onClickLogoutHandler} />
--- packages/client/src/shared/UI/Logo/index.tsx
+++ packages/client/src/shared/UI/Logo/index.tsx
@@ -1,4 +1,4 @@
-import { FC, HTMLAttributes } from 'react';
+import React, { FC, HTMLAttributes } from 'react';
 import cn from 'classnames';
 import { Images } from 'shared/images';
 import cl from './style.module.scss';
@@ -8,11 +8,15 @@ interface LogoProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 export const Logo: FC<LogoProps> = ({ className, dark, ...props }) => {
+	const onDragHandler = (e: React.DragEvent) => {
+		e.preventDefault();
+	};
+
 	return (
 		<div className={cn(cl.Logo, className, { [cl.Dark]: dark })} {...props}>
 			<h1>PI&emsp;&ensp;Cloud</h1>
-			<img src={Images.cloud} alt='Cloud' className={cl.CloudImage} />
-			<img src={Images.chick} alt='Chick' className={cl.ChickImage} />
+			<img src={Images.cloud} alt='Cloud' className={cl.CloudImage} onDragStart={onDragHandler} />
+			<img src={Images.chick} alt='Chick' className={cl.ChickImage} onDragStart={onDragHandler} />
 		</div>
 	);
 };
