4f04c720d02e888a3fcdb86681366c2ef8085db3
yvrxslvw <yvrxslvv@gmail.com>
2024-08-10 02:34:31 +0300

    Initialize database connection

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 const isLocal = process.env.APP_ENV === 'local';
 const isDev = process.env.APP_MODE === 'dev';
@@ -7,7 +8,19 @@ let envFilePath = isDev ? '.env.development' : '.env.production';
 if (isLocal) envFilePath += '.local';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath })],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true, envFilePath }),
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: process.env.PSQL_HOST,
+			port: Number(process.env.PSQL_PORT),
+			username: process.env.PSQL_USER,
+			password: process.env.PSQL_PASSWORD,
+			database: process.env.PSQL_DATABASE,
+			logging: isDev ? sql => console.log(sql) : false,
+			autoLoadModels: true,
+		}),
+	],
 	controllers: [],
 	providers: [],
 })
