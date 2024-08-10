661ecc35148cfb61257b09e00cda59381d7e8903
yvrxslvw <yvrxslvv@gmail.com>
2024-08-10 04:06:37 +0300

    Initialize database model

--- packages/server/src/users/entities/user.entity.ts
+++ packages/server/src/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+interface UserCreationAttributes {
+	login: string;
+	password: string;
+}
+
+@Table({ tableName: 'users', updatedAt: false })
+export class User extends Model<User, UserCreationAttributes> {
+	@Column({ type: DataType.INTEGER, primaryKey: true, autoIncrement: true })
+	declare id: number;
+
+	@Column({ type: DataType.STRING(24), allowNull: false, unique: true })
+	declare login: string;
+
+	@Column({ type: DataType.STRING(64), allowNull: false })
+	declare password: string;
+
+	declare createdAt: Date;
+}
--- packages/server/src/users/users.module.ts
+++ packages/server/src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
 
 @Module({
 	controllers: [UsersController],
 	providers: [UsersService],
+	imports: [SequelizeModule.forFeature([User])],
 })
 export class UsersModule {}
